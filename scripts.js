776df746d67ddc1d17d2ce3334a57027955e08e3
dkim3191 <dkim3191@gmail.com>
2025-02-18 21:34:13 -0500

    skeleton

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2);
+}
