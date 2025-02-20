342b53bd733b4f153d7e7bcfa4ab33246d133543
dkim3191 <dkim3191@gmail.com>
2025-02-20 13:02:16 -0500

    display clears after operator use. need to implement getting rid of 0 on entry and using operator logic

--- scripts.js
+++ scripts.js
@@ -15,19 +15,36 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1;
-let num2;
-let operator;
+let num1 = "0";
+let num2 = "";
+let operator = "";
 
 function operate(num1, num2, operator) {
     return operator(num1, num2);
+}
+
+function getOperator(btnID) {
+    switch (btnID) {
+        case "btn4":
+            return "plus";
+        case "btn8":
+            return "minus";
+        case "btn12":
+            return "times";
+        case "btn16":
+            return "divide";
+        case "btn15":
+            return "equals";
+    }
 }
 
 
 // Buttons
 const buttons = document.querySelector("#buttons");
 const display = document.querySelector("#display");
+display.textContent = "0";
 const textArray = [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, 'x', 'C', 0, '=', '/'];
+let newNum = true;
 
 for (let i = 1; i <= 16; i++) {
     const button = document.createElement("div");
@@ -44,16 +61,27 @@ for (let i = 1; i <= 16; i++) {
     button.addEventListener("click", (e) => {
         switch (e.target.classList[1]) {
             case "operator":
+                num1 = display.textContent;
+                operator = getOperator(e.target.id);
+                newNum = true;
                 break;
             case "clear":
-                display.textContent = "";
+                display.textContent = "0";
+                num1 = "";
+                num2 = "";
+                operator = "";
                 break;
             case "number":
+                if (operator != "" && newNum) {
+                    display.textContent = "";
+                    newNum = false;
+                }
                 if (display.textContent.length < 10) {
                     display.textContent += e.target.textContent;
                 }
                 break;
         }
+
 
     });
     buttons.appendChild(button);
